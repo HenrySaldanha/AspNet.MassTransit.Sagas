431edcc5f91d6b0b21daa46d83be1e7ed8821211
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-09-01 20:15:32 -0300

    feat: add state machine

---
+++ src/PurchaseSaga/OrderState.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Contrib.Extensions;
+using Domain.Models;
+using MassTransit;
+
+namespace PurchaseSaga;
+public class OrderState : SagaStateMachineInstance
+{
+    [ExplicitKey]
+    public Guid CorrelationId { get; set; }
+    public int CurrentState { get; set; }
+    public IEnumerable<Product> Products { get; set; }
+    public Customer SavedCustomer { get; set; }
+}
