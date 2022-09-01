bee064d58f8a7cfabec931fdf70ce03dfca37954
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-09-01 20:12:11 -0300

    feat: add commands contracts

---
+++ src/Domain/Commands.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+
+namespace Domain;
+
+public record CreateOrderCommand(Order OrderRequest);
+public record AddCustomerCommand(Guid CorrelationId, Customer Customer);
+public record AddProductsCommand(Guid CorrelationId, IEnumerable<Product> Products);
+public record RemoveCustomerCommand(Guid CorrelationId, Customer Customer);
