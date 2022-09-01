d74f8945afa21d454fc2f07f9f267725ee459e5c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-09-01 20:12:17 -0300

    feat: add events contracts

---
+++ src/Domain/Events.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+
+namespace Domain;
+
+public record CustomerAddedEvent(Guid CorrelationId, Customer Customer);
+public record ProductsAddedEvent(Guid CorrelationId);
+public record ProductsRejectedEvent(Guid CorrelationId);
+public record CustomerRemovedEvent(Guid CorrelationId);
