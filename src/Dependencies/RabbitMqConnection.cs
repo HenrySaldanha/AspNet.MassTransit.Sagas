a3ca57c0d4cb8cd3ddcea584f7a804e65abb6664
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-09-01 20:11:37 -0300

    feat: add dependencies project

---
+++ src/Dependencies/RabbitMqConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace Dependencies;
+
+public class RabbitMqConnection
+{
+    public const string HOST = "localhost";
+    public const string USERNAME = "guest";
+    public const string PASSWORD = "guest";
+}
---
+++ src/Dependencies/Worker.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Hosting;
+
+namespace Dependencies;
+
+public class Worker : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+            await Task.Delay(1000, stoppingToken);
+    }
+}
