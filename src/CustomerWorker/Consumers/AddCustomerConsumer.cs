d89192b401695a0036719f6437b3c658f73900ca
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-09-01 20:09:59 -0300

    feat: add customer consumers

---
+++ src/CustomerWorker/Consumers/AddCustomerConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Domain;
+using Serilog;
+
+namespace CustomerWorker.Consumers;
+
+public class AddCustomerConsumer : IConsumer<AddCustomerCommand>
+{
+    private readonly ILogger _logger;
+
+    public AddCustomerConsumer() => _logger = Log.ForContext<AddCustomerConsumer>();
+
+    public async Task Consume(ConsumeContext<AddCustomerCommand> context)
+    {
+        _logger.Information("Received event: {event}; Request: {@request}; CorrelationId {correlationId}",
+            nameof(AddCustomerCommand), @context.Message, context.CorrelationId);
+
+        Thread.Sleep(1000);
+
+        await context.Publish(new CustomerAddedEvent(context.Message.CorrelationId, context.Message.Customer));
+
+        _logger.Information("Published event: {event}; CorrelationId {correlationId}",
+            nameof(CustomerAddedEvent), context.CorrelationId);
+    }
+}
---
+++ src/CustomerWorker/Consumers/RemoveCustomerConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Domain;
+using Serilog;
+
+namespace CustomerWorker.Consumers;
+
+public class RemoveCustomerConsumer : IConsumer<RemoveCustomerCommand>
+{
+    private readonly ILogger _logger;
+
+    public RemoveCustomerConsumer() => _logger = Log.ForContext<RemoveCustomerConsumer>();
+
+    public async Task Consume(ConsumeContext<RemoveCustomerCommand> context)
+    {
+        _logger.Information("Received event: {event}; Request: {@request}; CorrelationId {correlationId}",
+            nameof(RemoveCustomerCommand), @context.Message, context.CorrelationId);
+
+        Thread.Sleep(1000);
+
+        await context.Publish(new CustomerRemovedEvent(context.Message.CorrelationId));
+
+        _logger.Information("Published event: {event}; CorrelationId {correlationId}",
+            nameof(CustomerRemovedEvent), context.CorrelationId);
+    }
+}
